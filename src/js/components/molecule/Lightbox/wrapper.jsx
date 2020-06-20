2a3843ac1275e842276f860ee752660deaa587b2
Luis Nabais <dextro@gmail.com>
2020-06-20 15:45:31 +0100

    Tweaks to gallery code

--- src/js/components/molecule/Lightbox/wrapper.jsx
+++ src/js/components/molecule/Lightbox/wrapper.jsx
@@ -1,54 +1,67 @@
 /** @jsx h */
 import { h } from 'preact'
-import { useCallback, useLayoutEffect, useRef, useState } from 'preact/hooks'
+import { useCallback, useLayoutEffect, useMemo, useState } from 'preact/hooks'
 import ReactImageLightbox from 'react-image-lightbox'
 
 import { ensureGroup } from './utils'
 import { LightboxProvider } from './context'
 
+// TODO: control gallery by hash
 function LightboxWrapper(props) {
   const { children } = props
 
-  const entriesRef = useRef({})
-  const [isOpen, setOpen] = useState(false)
-  const [currentGroup, setGroup] = useState(undefined)
-  const [currentIdx, setCurrent] = useState(0)
+  const [entries, setEntries] = useState({})
   const [sort, triggerSort] = useState(0)
+  const [state, setCurrent] = useState({
+    open: false,
+    group: undefined,
+    idx: undefined,
+  })
 
   const register = useCallback((id, data, group) => {
     group = ensureGroup(group)
 
-    entriesRef.current[group] = entriesRef.current[group] || []
-    entriesRef.current[group].push({
-      id,
-      data,
-    })
+    setEntries(entries => ({
+      ...entries,
+      [group]: (entries[group] || []).concat({
+        id,
+        data,
+      }),
+    }))
 
     triggerSort(c => c + 1)
-  }, [entriesRef, triggerSort])
+  }, [setEntries, triggerSort])
 
   const unregister = useCallback((id, group) => {
     group = ensureGroup(group)
 
-    if (entriesRef.current[group]) {
-      const idx = entriesRef.current[group].findIndex(entry => entry.id === id)
-      entriesRef.current[group].splice(idx, 1)
-    }
+    setEntries(entries => {
+      if (!entries[group]) {
+        return entries
+      }
 
-  }, [entriesRef])
+      return {
+        ...entries,
+        [group]: entries[group].filter(entry => entry.id !== id),
+      }
+    })
+  }, [setEntries])
 
   const open = useCallback((id, group) => {
     group = ensureGroup(group)
 
-    if (!entriesRef.current[group]) {
+    if (!entries[group]) {
       return
     }
 
-    const idx = entriesRef.current[group].findIndex(entry => entry.id === id)
-    setGroup(group)
-    setCurrent(idx)
-    setOpen(true)
-  }, [entriesRef, setGroup, setCurrent, setOpen])
+    const idx = entries[group].findIndex(entry => entry.id === id)
+
+    setCurrent({
+      open: true,
+      group,
+      idx,
+    })
+  }, [entries, setCurrent])
 
   useLayoutEffect(() => {
     const order = []
@@ -56,40 +69,62 @@ function LightboxWrapper(props) {
       order.push(node.dataset.lightboxId)
     }
 
-    for (const group of Object.values(entriesRef.current)) {
+    for (const group of Object.values(entries)) {
       group.sort((a, b) => {
         return order.indexOf(a.id) - order.indexOf(b.id)
       })
     }
-  }, [sort])
+  }, [entries, sort])
 
-  const entries = entriesRef.current[currentGroup] || []
-  const { data: currentEntry } = entries[currentIdx] || {}
-  const { data: nextEntry } = currentIdx < entries.length && entries[currentIdx + 1] || {}
-  const { data: prevEntry } = currentIdx > 0 && entries[currentIdx - 1] || {}
+  const currentEntries = entries[state.group] || []
+  const { data: currentEntry } = currentEntries[state.idx] || {}
+  const { data: nextEntry } = state.idx < currentEntries.length && currentEntries[state.idx + 1] || {}
+  const { data: prevEntry } = state.idx > 0 && currentEntries[state.idx - 1] || {}
 
-  const close = () => setOpen(false)
+  const close = useCallback(
+    () => setCurrent({ open: false }),
+    [setCurrent],
+  )
 
-  const ctxValue = {
-    register,
-    unregister,
-    open,
-    close,
-  }
+  const ctxValue = useMemo(
+    () => ({
+      register,
+      unregister,
+      open,
+      close,
+    }),
+    [register, unregister, open, close],
+  )
+
+  const onMoveNextRequest = useCallback(
+    () => setCurrent(state => ({
+      ...state,
+      idx: state.idx + 1,
+    })),
+    [setCurrent],
+  )
+
+  const onMovePrevRequest = useCallback(
+    () => setCurrent(state => ({
+      ...state,
+      idx: state.idx - 1,
+    })),
+    [setCurrent],
+  )
 
   return (
     <LightboxProvider value={ctxValue}>
       {children}
 
-      {isOpen && <ReactImageLightbox
+      {state.open && <ReactImageLightbox
         mainSrc={currentEntry.src}
         nextSrc={nextEntry && nextEntry.src}
         prevSrc={prevEntry && prevEntry.src}
         imageCaption={currentEntry.caption}
         imagePadding={80}
         onCloseRequest={close}
-        onMoveNextRequest={nextEntry && (() => setCurrent(current => current + 1))}
-        onMovePrevRequest={prevEntry && (() => setCurrent(current => current - 1))}
+        onMoveNextRequest={nextEntry && onMoveNextRequest}
+        onMovePrevRequest={prevEntry && onMovePrevRequest}
       />}
     </LightboxProvider>
   )
